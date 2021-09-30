9825c93614cc012e0f01205305c3c5ccd2214323
Lucas Lima <lucaslima.eng2311@gmail.com>
2021-09-29 21:46:17 -0300

    Programa principal e classes da atividade 1 da trilha de especialistas em automacao

---
+++ Class1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Atividade_final___Trilha_especialista
+{
+    class Class1
+    {
+        class Funcionario // Classe mãe
+        {
+            public string Nome { get; set; }
+            public int Carga_horaria { get; set; }
+            public double Valor_hora { get; set; }
+            public double Salario { get; set; }
+            public int Tipo_funcionario { get; set; }
+        }
+        class Funcionario_terceiro : Funcionario  // Classe filha
+        {
+
+            public string Empresa_origem { get; set; }
+            public double Taxa_servico { get; private set; }
+
+            public Funcionario_terceiro(string Nome_t, int Carga_horaria_t, double Valor_hora_t, double Salario_t)
+            {
+                Empresa_origem = Empresa_origem;
+                Taxa_servico = Taxa_servico;
+                Nome_t = Nome;
+                Carga_horaria_t = Carga_horaria;
+                Valor_hora_t = Valor_hora;
+                Salario_t = Salario;
+            }
+            public void CalculaSalario()   // method
+            {
+                if (Tipo_funcionario == 0)
+                {
+                    Console.WriteLine("Funcionario Interno!");
+                    Salario = Carga_horaria * Valor_hora;
+                }
+                else
+                {
+                    Console.WriteLine("Funcionario Terceiro!");
+                    Salario = (Carga_horaria * Valor_hora) + Taxa_servico;
+                }
+
+            }
+            public void ImprimeDados()   // method
+            {
+                Console.WriteLine("O nome do funcionário é: " + Nome);
+                Console.WriteLine("O salário do funcionário + " + Nome + "é: " + Salario);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Atividade_final___Trilha_especialista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teste");
+            Console.WriteLine("Por favor, digite 1 se o funcionario for terceirizado, ou 0 se for interno");
+            int tipo_func = Console.ReadLine();
+            Funcionario_terceiro func1 = new Funcionario_terceiro("Lucas", 40, 8, 4350);
+            func1.CalculaSalario();
+            func1.ImprimeDados(); 
+        }
+    }
+}
